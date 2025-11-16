c3ab6fcafbf9c59be1e3cce21d4203ab714a59ee
BOTC3PO <74213407+BOTC3PO@users.noreply.github.com>
2025-11-16 04:20:21 -0300

    estructura epica generada

---
+++ apps/web/src/generador/matematicas/MatematicasBasicas.tsx
@@ -0,0 +1,8 @@
+let resultado,opciones=['suma','resta','multiplicacion','divicion'];
+
+export default function generador_basico(rango_min:number,rango_maximo:number,opcion:Int8Array,) {
+    
+
+
+return(resultado)    
+}
