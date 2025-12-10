ad0cf2e13455051b10f56609d9e37dd95ea31652
BOTC3PO <74213407+BOTC3PO@users.noreply.github.com>
2025-12-10 18:29:27 -0300

    actualizacion de dependencias

--- apps/web/eslint.config.js
+++ apps/web/eslint.config.js
@@ -1,23 +1,65 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
+//import js from '@eslint/js'
+//import globals from 'globals'
+//import reactHooks from 'eslint-plugin-react-hooks'
+//import reactRefresh from 'eslint-plugin-react-refresh'
+//import tseslint from 'typescript-eslint'
+//import { globalIgnores } from 'eslint/config'
+//
+//export default tseslint.config([
+//  globalIgnores(['dist']),
+//  {
+//    files: ['**/*.{ts,tsx}'],
+//    extends: [
+//      js.configs.recommended,
+//      tseslint.configs.recommended,
+//      reactHooks.configs['recommended-latest'],
+//      reactRefresh.configs.vite,
+//    ],
+//    languageOptions: {
+//      ecmaVersion: 2020,
+//      globals: globals.browser,
+//    },
+//  },
+//])
+//
 
-export default tseslint.config([
-  globalIgnores(['dist']),
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+
+export default [
   {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
+    files: ["**/*.{js,jsx,ts,tsx}"],
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+      ecmaVersion: 2022,
+      sourceType: "module",
+      globals: {
+        ...globals.browser,
+        ...globals.es2021
+      },
+      parser: tseslint.parser,
+      parserOptions: {
+        ecmaFeatures: { jsx: true }
+      }
     },
-  },
-])
+    plugins: {
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...tseslint.configs.recommendedTypeChecked[0].rules,
+
+      // React Hooks (obligatorio para apps React modernas)
+      ...reactHooks.configs.recommended.rules,
+
+      // Vite + React Fast Refresh
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true }
+      ]
+    }
+  }
+];
